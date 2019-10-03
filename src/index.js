37796b1a825c784cf9810a79bb29662d30df67e8
wilantury <ventas@antury.net>
2019-10-02 22:52:50 -0500

    Revisión final de la solución a los problemas 1,2,3 y 4 del reto 5 EJS

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-//const API = 'https://rickandmortyapi.com/api/character/';
-const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
+const API = 'https://rickandmortyapi.com/api/character/';
 
 const miStorage = window.localStorage;  
 miStorage.clear()
-
 const builSection = (response) =>{
   const characters = response.results;
       if(response.info.next === ''){
         miStorage.setItem('next_fetch', API);
-        console.log(`Ya no hay mas personajes`)
         intersectionObserver.disconnect()
+        var capa = document.getElementById("message");
+        capa.innerHTML = "<h1>No hay más imágenes<h1/>";
       }else{
         miStorage.setItem('next_fetch', response.info.next);
       }      
-      console.log(characters.info)
       let output = characters.map(character => {
         return `
       <article class="Card">
@@ -30,9 +28,7 @@ const builSection = (response) =>{
       $app.appendChild(newItem);
 }
 
-
 async function getData (api){
-
   try{
     const responseFetch = await fetch(api)
     const response = await responseFetch.json()    
@@ -42,32 +38,8 @@ async function getData (api){
   }
 }
 
-/* const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
-      miStorage.setItem('next_fetch', response.info.next);
-
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-} */
-
 const loadData = () => {
-  const API_NEXT = miStorage.getItem('next_fetch');
-  
+  const API_NEXT = miStorage.getItem('next_fetch');  
   if(API_NEXT){
     getData(API_NEXT);
   }else{
